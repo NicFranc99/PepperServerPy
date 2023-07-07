129710567afaf38229569d4d9767c3d70bc84dcd
NicFranc99 <nidafraeli@live.it>
2023-07-07 12:03:11 +0000

    fix ommit

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Ciao sono stato deployato!"
+
+    if __name__ == '__main__':
+        app.run(debug= true,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
