76ba1a8a00ee8257810bb034d43d593d9bd84b91
NicFranc99 <nidafraeli@live.it>
2023-07-07 23:24:56 +0000

    Esposto api per il calcolo della similarita' e del checking se la parola in ingresso esiste nel vocabolario italiano oppure no.

--- app.py
+++ app.py
@@ -1,6 +1,48 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import openai
+from sklearn.metrics.pairwise import cosine_similarity
+import italian_dictionary
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Ciao sono stato deployato!"
+openai.api_key = "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"
+model = "text-davinci-003"
+
+@app.route("/openAi/textSimilarity", methods=['GET'])
+def textSimilarityCheck():
+     input_json = request.get_json(force=True) 
+     category = input_json['category']
+     text = input_json['text']
+     result = calculate_cosine_similarity(category, text)
+     return jsonify(result)
+ 
+def calculate_cosine_similarity(category,text):
+    response = openai.Completion.create(
+        model=model,
+        #prompt=f"semantic similarity cosin coefficient between: \n{category},\n{text}",
+        prompt=f"Coefficiente similarita' semantica del coseno tra: \n{category},\n{text}. Rispondi solo con il valore float",
+         temperature=0,
+         max_tokens=64,
+         top_p=1.0,
+         frequency_penalty=0.0,
+         presence_penalty=0.0
+         )
+    
+    return response
+
+
+@app.route("/openAi/wordExists", methods=['GET'])
+def wordExistsCheck():
+     input_json = request.get_json(force=True) 
+     text = input_json['text']
+     result = check_if_word_in_dictionary(text)
+     return jsonify(result)
+
+def check_if_word_in_dictionary(word):
+    try:
+        result = italian_dictionary.get_definition(word, limit=3, all_data=False) 
+        return True
+    except:
+        return False
+
+    
